70d0f92d6095fbcea85e57323cf9bf079aa8336e
Daniil Kazantsev <35533685+rhtfl@users.noreply.github.com>
2020-09-18 11:20:27 +0300

    1

---
+++ precache-manifest.0cb9f89a36df773b36f09ab7965ab512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04358b8994fa14d5edede4930d0675b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f0c5723acf95b9aebce",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "e341e2deb3e0e5361af0",
+    "url": "/static/css/main.732cb669.chunk.css"
+  },
+  {
+    "revision": "1f0c5723acf95b9aebce",
+    "url": "/static/js/2.a8c3a341.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8c3a341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e341e2deb3e0e5361af0",
+    "url": "/static/js/main.9f764946.chunk.js"
+  },
+  {
+    "revision": "31071158c1d517bc05af",
+    "url": "/static/js/runtime-main.f220af85.js"
+  }
+]);
